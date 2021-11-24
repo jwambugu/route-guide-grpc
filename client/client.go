c1006b3e808998e6e4cac1cf44084f9f9a5c4e7c
Joram Wambugu <artisan254@gmail.com>
2021-11-24 10:11:43 +0300

    Print features from the server

--- client/client.go
+++ client/client.go
@@ -161,19 +161,19 @@ func main() {
 
 	client := routeguide.NewRouteGuideClient(conn)
 
-	//// Looking for a valid feature
-	//printFeature(client, &routeguide.Point{Latitude: 409146138, Longitude: -746188906})
-	//
-	//// Looking for a missing feature
-	//printFeature(client, &routeguide.Point{Latitude: 0, Longitude: 0})
-	//
-	//// Looking for features between 40, -75 and 42, -73.
-	//printFeatures(client, &routeguide.Rectangle{
-	//	Lo: &routeguide.Point{Latitude: 400000000, Longitude: -750000000},
-	//	Hi: &routeguide.Point{Latitude: 420000000, Longitude: -730000000},
-	//})
+	// Looking for a valid feature
+	printFeature(client, &routeguide.Point{Latitude: 409146138, Longitude: -746188906})
 
-	//runRecordRoute(client)
+	// Looking for a missing feature
+	printFeature(client, &routeguide.Point{Latitude: 0, Longitude: 0})
+
+	// Looking for features between 40, -75 and 42, -73.
+	printFeatures(client, &routeguide.Rectangle{
+		Lo: &routeguide.Point{Latitude: 400000000, Longitude: -750000000},
+		Hi: &routeguide.Point{Latitude: 420000000, Longitude: -730000000},
+	})
+
+	runRecordRoute(client)
 
 	runRouteChat(client)
 }
